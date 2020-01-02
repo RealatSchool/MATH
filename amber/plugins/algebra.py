40df72dcdceda9d672149831f88a387f79a036cd
RealatSchool <51348599+RealatSchool@users.noreply.github.com>
2020-01-02 13:35:00 -0500

    streamline

--- amber/plugins/algebra.py
+++ amber/plugins/algebra.py
@@ -7,20 +7,20 @@ class AlgebraSession:
         self.client = client
         self.config = config
 	
-    
+	
     async def respond(self, message):
         #split to get the equation and then turn it from a string into something that sympy can understand
         c = message.content.strip().split()
         answers = sympy.solve(sympy.parsing.sympy_parser.parse_expr(c[2]), c[3])
         #check for answers and send them if there are any
         if answer.len == 0:
-            await self.client.send("There are no solutions!", message.channel)
+	    response = "There are no solutions!"
         else:
             response = "The answers are "
             for answer in answers:
                 response = response + answer + ", "
             response = response[:-2]
-            await self.client.send(response, message.channel)
+	await self.client.send(response, message.channel)
 
 def load(client, config):
-    return AboutSession(client, config)
+    return AlgebraSession(client, config)
