3980246bd1ee56f8891fb6d3cc5d6a8867a93a7f
RealatSchool <51348599+RealatSchool@users.noreply.github.com>
2020-01-03 13:06:19 -0500

    check if command is valid

--- amber/plugins/algebra.py
+++ amber/plugins/algebra.py
@@ -11,15 +11,18 @@ class AlgebraSession:
     async def respond(self, message):
         #split to get the equation and then turn it from a string into something that sympy can understand
         c = message.content.strip().split()
-        answers = sympy.solve(sympy.parsing.sympy_parser.parse_expr(c[2]), c[3])
-        #check for answers and send them if there are any
-        if answer.len == 0:
-	    response = "There are no solutions!"
-        else:
-            response = "The answers are "
-            for answer in answers:
-                response = response + answer + ", "
-            response = response[:-2]
+	if len(c) != 4:
+	    response = "The format of this command is <equation> <variable to solve for>!"
+	else:
+            answers = sympy.solve(sympy.parsing.sympy_parser.parse_expr(c[2]), c[3])
+            #check for answers and send them if there are any
+            if answer.len == 0:
+	        response = "There are no solutions!"
+            else:
+                response = "The answers are "
+                for answer in answers:
+                    response = response + answer + ", "
+                response = response[:-2]
 	await self.client.send(response, message.channel)
 
 def load(client, config):
