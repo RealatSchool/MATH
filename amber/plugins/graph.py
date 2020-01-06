4ae6905fc5a6759c71dd579ffb5613d620a9822f
Real Awesomeness <RealAwesomeness@outlook.com>
2020-01-06 14:26:48 -0500

    spaces and message fix

--- amber/plugins/graph.py
+++ amber/plugins/graph.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plot
 import sympy
 import StringIO
+import discord
 
 class GraphSession:
 
@@ -12,9 +13,9 @@ class GraphSession:
     def graph(function, variable):
         density = config["graph"]["density"] #number of points per unit
         scale = config["graph"]["scale"] #number of units per axe
-	    for point in range(density * scale["x"]): #iterate through all the points in the plot defined by the scale and density
+        for point in range(density * scale["x"]): #iterate through all the points in the plot defined by the scale and density
 	        plot(point/density,sympy.solve(sympy.parsing.sympy_parser.parse_expr(function.replace("x",str(point/density))), variable)) #set the y coordinate equal to the sympy answer for that x value
-	    
+
         image_data = StringIO.StringIO()
         plot.savefig(image_data, format='jpg')
         image_data.seek(0)
@@ -23,7 +24,7 @@ class GraphSession:
     async def respond(self, message):
         #split to get the equation and variable to pass to the graph function
         c = message.content.strip().split()
-	    if len(c) != 4:
+        if len(c) != 4:
 	        response = "The format of this command is <equation> <variable to solve for>!"
         c[2] = c[2].replace("=", "").replace("f(x)", "").replace("y", "")
         try:
@@ -31,7 +32,7 @@ class GraphSession:
         except:
             await self.client.send("An error occurred! Please try again.")
         else:
-	        await self.client.send(file=picture, message.channel)
+	        await self.client.send(file=picture, channel=message.channel)
 
 def load(client, config):
     return GraphSession(client, config)
