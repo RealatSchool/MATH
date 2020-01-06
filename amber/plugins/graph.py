542cf1ebe8bd5f7923ba386fd052eaae91380fbd
RealatSchool <51348599+RealatSchool@users.noreply.github.com>
2020-01-06 13:21:38 -0500

    Update graph.py

--- amber/plugins/graph.py
+++ amber/plugins/graph.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plot
 import sympy
+import StringIO
 
 class GraphSession:
 
@@ -8,26 +9,29 @@ class GraphSession:
         self.client = client
         self.config = config
 	  
-    def graph(function):
-        density = config["graph"]["density"]
-        scale = config["graph"]["scale"]
-	
+    def graph(function, variable):
+        density = config["graph"]["density"] #number of points per unit
+        scale = config["graph"]["scale"] #number of units per axe
+	    for point in range(density * scale["x"]): #iterate through all the points in the plot defined by the scale and density
+	        plot(point/density,sympy.solve(sympy.parsing.sympy_parser.parse_expr(function.replace("x",str(point/density))), variable)) #set the y coordinate equal to the sympy answer for that x value
+	    
+        image_data = StringIO.StringIO()
+        plot.savefig(image_data, format='jpg')
+        image_data.seek(0)
+        return image_data
+
     async def respond(self, message):
-        #split to get the equation and then turn it from a string into something that sympy can understand
+        #split to get the equation and variable to pass to the graph function
         c = message.content.strip().split()
-	if len(c) != 4:
-	    response = "The format of this command is <equation> <variable to solve for>!"
-	else:
-            answers = sympy.solve(sympy.parsing.sympy_parser.parse_expr(c[2]), c[3])
-            #check for answers and send them if there are any
-            if answer.len == 0:
-	        response = "There are no solutions!"
-            else:
-                response = "The answers are "
-                for answer in answers:
-                    response = response + answer + ", "
-                response = response[:-2]
-	await self.client.send(response, message.channel)
+	    if len(c) != 4:
+	        response = "The format of this command is <equation> <variable to solve for>!"
+        c[2] = c[2].replace("=", "").replace("f(x)", "").replace("y", "")
+        try:
+            picture = discord.File(graph(c[2], c[3])
+        except:
+            await self.client.send("An error occurred! Please try again.")
+        else:
+	        await self.client.send(file=picture, message.channel)
 
 def load(client, config):
     return GraphSession(client, config)
