20473904c9d105b43b524baa3d5ec4320bad648b
Real Awesomeness <RealAwesomeness@outlook.com>
2020-01-07 17:35:01 -0500

    trying to fix graphing

--- amber/plugins/graph.py
+++ amber/plugins/graph.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plot
-import sympy
+from sympy import *
 from io import StringIO
 import discord
+from sympy.parsing.sympy_parser import parse_expr, standard_transformations, implicit_multiplication_application, convert_xor
 
 class GraphSession:
 
@@ -9,12 +10,13 @@ class GraphSession:
     def __init__(self, client, config):
         self.client = client
         self.config = config
+        self.transformations = (standard_transformations + (implicit_multiplication_application,) + (convert_xor,))
 	  
-    def graph(function, variable):
-        density = config["graph"]["density"] #number of points per unit
-        scale = config["graph"]["scale"] #number of units per axe
+    def graph(self, f, variable):
+        density = self.config["density"] #number of points per unit
+        scale = self.config["scale"] #number of units per axe
         for point in range(density * scale["x"]): #iterate through all the points in the plot defined by the scale and density
-	        plot(point/density,sympy.solve(sympy.parsing.sympy_parser.parse_expr(function.replace("x",str(point/density))), variable)) #set the y coordinate equal to the sympy answer for that x value
+	        plot(point/density, solve(parse_expr(f.replace("x",str(round(point/density,2)))), transformations=self.transformations)) #set the y coordinate equal to the sympy answer for that x value
 
         image_data = StringIO.StringIO()
         plot.savefig(image_data, format='jpg')
@@ -28,11 +30,11 @@ class GraphSession:
 	        response = "The format of this command is <equation> <variable to solve for>!"
         c[2] = c[2].replace("=", "").replace("f(x)", "").replace("y", "")
         try:
-            picture = discord.File(graph(c[2], c[3]))
+            picture = discord.File(self.graph(c[2], c[3]))
         except:
-            await self.client.send("An error occurred! Please try again.")
+            await self.client.send("An error occurred! Please try again.", message.channel)
         else:
-	        await self.client.send(file=picture, channel=message.channel)
+	        await message.channel.send(file=picture)
 
 def load(client, config):
     return GraphSession(client, config)
