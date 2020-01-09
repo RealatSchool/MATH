1bb6c2c7866df6cf736fc58d970056ae06a7d4db
Real Awesomeness <RealAwesomeness@outlook.com>
2020-01-09 12:42:20 -0500

    graph fixing - load it into a webserver

--- amber/plugins/graph.py
+++ amber/plugins/graph.py
@@ -8,6 +8,8 @@ from io import BytesIO, StringIO
 import discord
 from sympy.parsing.sympy_parser import parse_expr, standard_transformations, implicit_multiplication_application, convert_xor
 import shutil
+import string
+import os
 
 class GraphSession:
 
@@ -23,11 +25,15 @@ class GraphSession:
         ax=fig.add_subplot(111)
         for point in range(density * scale["x"]): #iterate through all the points in the plot defined by the scale and density
 	        ax.plot(point/density, parse_expr(f.replace(variable,str(round(point/density,2)))).evalf()) #set the y coordinate equal to the sympy evaluation for that x value
+        exists = True
+        while exists:
+            filename = ''.join(random.choice(string.ascii_letters) for i in range(15)) + ".png"
+            if os.path.exists(self.config["save_location"] + filename):
+                exists = False
+        
+        fig.savefig(self.config["save_location"] + filename, dpi='figure', format='png')
 
-        buffer = BytesIO()
-        fig.savefig(buffer, dpi='figure', format='png')
-
-        return buffer
+        return filename
 
     async def respond(self, message):
         #split to get the equation and variable to pass to the graph function
@@ -36,12 +42,12 @@ class GraphSession:
 	        await self.client.send("The format of this command is <equation> <variable to solve for>!", message.channel)
         else:
             c[1] = c[1].replace("=", "").replace("f(" + c[2] + ")", "").replace("y", "")
-            #try:
-            picture = discord.File(fp=self.graph(c[1], c[2]), filename="graph.png")
-            #except:
-            #    await self.client.send("An error occurred! Please try again.", message.channel)
-            #else:
-            await self.client.send_file(picture, message.channel)
+            try:
+                filename = discord.File(fp=self.graph(c[1], c[2]), filename="graph.png")
+            except:
+                await self.client.send("An error occurred! Please try again.", message.channel)
+            else:
+                await self.client.send(self.config["web_address"] + filename, message.channel)
 
 def load(client, config):
     return GraphSession(client, config)
