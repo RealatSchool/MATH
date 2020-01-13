6619837c08474984c930a18248c618a53317e3ae
RealatSchool <51348599+RealatSchool@users.noreply.github.com>
2020-01-13 11:56:50 -0500

    read

--- amber/plugins/read.py
+++ amber/plugins/read.py
@@ -1,1 +1,27 @@
+import ../tools/ocr
+import requests
+from io import BytesIO
 
+class ReadSession:
+
+
+    def __init__(self, client, config):
+        self.client = client
+        self.config = config
+	
+	
+    async def respond(self, message):
+        response = "The image(s) say: ```"
+        count = 0
+        for image in message.attachments:
+            for extension in self.config["file_types"]:
+                if image["url"].endswith(extension):
+                    count += 1
+                    #get the url from the message attachment, send it to bytes io and then open it as an image. Then, send to OCRTool
+                    response = response + "``` - Image " + str(count) + "\n\n" + ocr.OCRTool.readImage(Image.open(BytesIO(requests.get(image["url"]).content)))
+        response = response.replace("*", "\*")
+        
+        await self.client.send(response, message.channel)
+
+def load(client, config):
+    return AlgebraSession(client, config)
